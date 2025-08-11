012c25203a2f853480eba0af39bcd6db8a961844
codersantanu <codingwithsantanu308@gmail.com>
2025-08-11 22:25:44 +0530

    Setting the background color

--- alien_invation.py
+++ alien_invation.py
@@ -13,7 +13,8 @@ class AlienInvasion:
         
         # Set the title text for the window
         pygame.display.set_caption("Alien Invasion")
-    
+
+        self.bg_color=(230,230,230)#Set the backgroung color
     
     def run_game(self):
         # Infinite loop that keeps the game running until the user quits
@@ -25,8 +26,11 @@ class AlienInvasion:
                 # If the user clicked the close button on the window
                 if event.type == pygame.QUIT:
                     sys.exit()  # Exit the program immediately
-
-
+                    
+            #Redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
 # Run the game only if this file is executed directly
 if __name__ == '__main__':
     ai = AlienInvasion()  # Create an instance of the game
