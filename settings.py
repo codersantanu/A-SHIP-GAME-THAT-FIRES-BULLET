9063bcb88129803d319076f17236f59098ee32a9
codersantanu <codingwithsantanu308@gmail.com>
2025-08-12 19:08:52 +0530

    Add Settings class to manage game configuration

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    #This class store all settings for Allien Invention.
+    
+    def __init__(self):
+        """Initialize the game setting"""
+        
+        #Screen Setting
+        self.screen_width=1200
+        self.screen_hight=800
+        self.bg_color=(230,230,230)
+        
