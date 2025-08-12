8a9ad0bcc222ddd81e322b635ba66aa515115967
codersantanu <codingwithsantanu308@gmail.com>
2025-08-12 19:24:50 +0530

    Import Setting into the main programme

--- alien_invation.py
+++ alien_invation.py
@@ -1,15 +1,16 @@
 import sys         # Lets us exit the game cleanly when needed
 import pygame      # Main library for making the game window, graphics, and handling events
-
+from settings import Settings
 
 class AlienInvasion:
     """Overall class that manages game assets and behavior"""  # Description of what this class does
     
     def __init__(self):
         pygame.init()   # Initialize all Pygame modules (graphics, sound, etc.)
-        
-        # Create the game window with width=1200, height=800 pixels
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings=Settings()
+        # Create the game window with
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_hight))
         
         # Set the title text for the window
         pygame.display.set_caption("Alien Invasion")
@@ -28,7 +29,7 @@ class AlienInvasion:
                     sys.exit()  # Exit the program immediately
                     
             #Redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 # Run the game only if this file is executed directly
