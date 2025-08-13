5861fb00d1609226c390e388bc21e172bc95b028
codersantanu <codingwithsantanu308@gmail.com>
2025-08-13 20:33:21 +0530

    The ship image  for alien invention

---
+++ ship.py
@@ -0,0 +1,17 @@
+import pygame
+class Ship:
+    def __init__(self,ai_game):
+        """Initializing sheep and its starting position"""
+        self.screen=ai_game.screen
+        self.screen_react=ai_game.screen.get_rect()
+        
+        #Load the ship immage and get its rect.
+        self.image=pygame.image.load('image/ship.bmp')
+        self.rect=self.image.get_rect()
+        
+        #Start each new ship at the bottom center of the screen.
+        self.rect.midbottom=self.screen_react.midbottom
+        
+    def blitme(self):
+            """Draw the shit at its current location"""
+            self.screen.blit(self.image,self.rect)
