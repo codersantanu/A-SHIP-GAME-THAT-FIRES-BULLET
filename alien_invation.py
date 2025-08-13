a4bbf376995bb9f9dc23e0e8cb60f00f2fd66ffc
codersantanu <codingwithsantanu308@gmail.com>
2025-08-13 21:25:24 +0530

    Draw the ship to the main screen

--- alien_invation.py
+++ alien_invation.py
@@ -1,6 +1,7 @@
-import sys         # Lets us exit the game cleanly when needed
-import pygame      # Main library for making the game window, graphics, and handling events
-from settings import Settings
+import sys              # Lets us exit the game cleanly when needed
+import pygame           # Main library for making the game window, graphics, and handling events
+from ship import Ship   # Import the Ship class
+from settings import Settings   # Import the Setting class
 
 class AlienInvasion:
     """Overall class that manages game assets and behavior"""  # Description of what this class does
@@ -15,7 +16,7 @@ class AlienInvasion:
         # Set the title text for the window
         pygame.display.set_caption("Alien Invasion")
 
-        self.bg_color=(230,230,230)#Set the backgroung color
+        self.ship=Ship(self) #from Ship class
     
     def run_game(self):
         # Infinite loop that keeps the game running until the user quits
@@ -30,6 +31,8 @@ class AlienInvasion:
                     
             #Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme() 
+            
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 # Run the game only if this file is executed directly
